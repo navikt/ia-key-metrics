62ba82633023738c86dbae436cf325394e54b55c
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2023-06-05 13:42:19 +0200

    Fiks feil at kunne ikke finne index.html (endringen fra team nada)

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 quarto render index.qmd
 
-curl -X PUT -F file=@index.html \
+curl -X PUT -F index.html=@index.html \
     https://${NADA_HOST}/quarto/update/${QUARTO_ID} \
     -H "Authorization:Bearer ${TEAM_TOKEN}"
