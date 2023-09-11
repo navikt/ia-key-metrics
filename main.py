8f66f4f2c7fd05450ff615ca1e4fa5d8c0c4cb50
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2023-09-11 14:53:32 +0200

    Bytt til nye key metrics
    Co-authored-by: Runar Skagestad <runska@gmail.com>

--- main.py
+++ main.py
@@ -12,68 +12,42 @@ def load_data():
     return bq_client.query(query=config.SQL_QUERY).to_dataframe()
 
 
-def calculate_key_metrics(data, startdato, sluttdato):
-    data["målgruppe"] = (data.antall_ansatte > 1) & (data.antall_ansatte <= 50)
-    data["opprettet_dato"] = data["opprettet"].dt.date
-
-    data_intervall = data[(data.opprettet > startdato) & (data.opprettet <= sluttdato)]
-    data_utenfor_intervall = data[(data.opprettet <= startdato)]
-
-    # Antall brukere
-    brukere_intervall = data_intervall.orgnr.unique().tolist()
-    brukere_utenfor_intervall = data_utenfor_intervall.orgnr.unique().tolist()
-
-    # Antall tilbakevendende brukere
-    tilbakevendende_brukere = len(
-        set(brukere_intervall).intersection(brukere_utenfor_intervall)
-    )
-
-    # Antall brukere i målgruppen
-    brukere_målgruppen = data_intervall[data_intervall.målgruppe].orgnr.nunique()
-
-    # Antall leverte IA-tjenester til målgruppen
-    ia_tjenester_målgruppen = (
-        data_intervall[data_intervall.målgruppe].drop_duplicates(
-            ["orgnr", "opprettet_dato"]
+def calculate_key_metrics(data_raw, startdato, sluttdato):
+    data = (
+            data_raw[data_raw.kilde_applikasjon=="FOREBYGGINGSPLAN"]
+            .assign(opprettet_date=data_raw["opprettet"].dt.date)
+            .sort_values(by=["orgnr", "opprettet"])
+            .drop_duplicates(subset=["orgnr", "opprettet_date"])
         )
-    ).shape[0]
+    count = data[(data.opprettet > startdato) & (data.opprettet <= sluttdato)].orgnr.value_counts()
 
-    return tilbakevendende_brukere, brukere_målgruppen, ia_tjenester_målgruppen
+    antall_åpnet_kort = count.count()
+    antall_åpnet_kort_flere_dager = (count>=2).sum()
+
+    return antall_åpnet_kort, antall_åpnet_kort_flere_dager
 
 
 def plot_key_metrics(data, antall_dager: int):
     sluttdato = datetime.now()
     startdato = sluttdato - timedelta(days=antall_dager)
-    (
-        tilbakevendende_brukere,
-        brukere_målgruppen,
-        ia_tjenester_målgruppen,
-    ) = calculate_key_metrics(data, startdato, sluttdato)
-    (
-        ref_tilbakevendende_brukere,
-        ref_brukere_målgruppen,
-        ref_ia_tjenester_målgruppen,
-    ) = calculate_key_metrics(
-        data, startdato - timedelta(days=30), sluttdato - timedelta(days=30)
-    )
+    antall_åpnet_kort, antall_åpnet_kort_flere_dager = calculate_key_metrics(data, startdato, sluttdato)
 
     fig = sp.make_subplots(
-        rows=1,
-        cols=3,
+        rows=2,
+        cols=1,
         specs=[
-            [{"type": "indicator"}, {"type": "indicator"}, {"type": "indicator"}],
+            [{"type": "indicator"}], [{"type": "indicator"}],
         ],
     )
 
     fig.add_trace(
         go.Indicator(
-            mode="number+delta",
-            value=tilbakevendende_brukere,
-            delta={"reference": ref_tilbakevendende_brukere},
+            mode="number",
+            value=antall_åpnet_kort,
             number={"valueformat": ", g"},
             title={
                 "text": "<br><span style='font-size:0.7em;color:gray'>{0}</span>".format(
-                    "Tilbakevendende brukere"
+                    "Antall virksomheter som har åpnet panel"
                 )
             },
         ),
@@ -83,34 +57,41 @@ def plot_key_metrics(data, antall_dager: int):
 
     fig.add_trace(
         go.Indicator(
-            mode="number+delta",
-            value=brukere_målgruppen,
-            delta={"reference": ref_brukere_målgruppen},
+            mode="number+gauge+delta",
+            gauge = {'shape': "bullet"},
+            delta = {'reference': 50},
+            value=antall_åpnet_kort_flere_dager,
             number={"valueformat": ", g"},
             title={
                 "text": "<br><span style='font-size:0.7em;color:gray'>{0}</span>".format(
-                    "Brukere i målgruppen"
+                    "Antall virksomheter som har åpnet panel flere dager"
                 )
             },
         ),
-        row=1,
-        col=2,
+        row=2,
+        col=1,
+    )
+    return fig
+
+
+def plot_frequency_forebyggingsplan(data_raw):
+    data = (
+        data_raw[data_raw.kilde_applikasjon=="FOREBYGGINGSPLAN"]
+        .assign(opprettet_date=data_raw["opprettet"].dt.date)
+        .sort_values(by=["orgnr", "opprettet"])
+        .drop_duplicates(subset=["orgnr", "opprettet_date"])
     )
 
-    fig.add_trace(
-        go.Indicator(
-            mode="number+delta",
-            value=ia_tjenester_målgruppen,
-            delta={"reference": ref_ia_tjenester_målgruppen},
-            number={"valueformat": ", g"},
-            title={
-                "text": "<br><span style='font-size:0.7em;color:gray'>{0}</span>".format(
-                    "Leverte IA-tjenester til målgruppen"
-                )
-            },
-        ),
-        row=1,
-        col=3,
-    )
+    idag = datetime.now()
+    count = data[data.opprettet > idag - timedelta(days=30)].orgnr.value_counts()
 
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(x=count))
+    fig.update_layout(
+        height=400,
+        width=500,
+        title="Antall dager virksomheter brukte planen",
+        xaxis_title="Antall dager",
+        yaxis_title="Antall virksomheter",
+    )
     return fig
