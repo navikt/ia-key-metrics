fe3abe4605f860d5f7ee19aed1ae16f5cb75a150
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2023-09-12 08:29:50 +0200

    Oppdater design

--- main.py
+++ main.py
@@ -32,43 +32,60 @@ def plot_key_metrics(data, antall_dager: int):
     startdato = sluttdato - timedelta(days=antall_dager)
     antall_åpnet_kort, antall_åpnet_kort_flere_dager = calculate_key_metrics(data, startdato, sluttdato)
 
-    fig = sp.make_subplots(
-        rows=2,
-        cols=1,
-        specs=[
-            [{"type": "indicator"}], [{"type": "indicator"}],
-        ],
-    )
+    text_format = "<br><span style='font-size:1em;color:gray'>{0}<br><span style='font-size:0.7em;color:gray'>{1}"
 
+    fig = go.Figure()
     fig.add_trace(
         go.Indicator(
             mode="number",
             value=antall_åpnet_kort,
-            number={"valueformat": ", g"},
-            title={
-                "text": "<br><span style='font-size:0.7em;color:gray'>{0}</span>".format(
-                    "Antall virksomheter som har åpnet panel"
-                )
-            },
+            number={"valueformat": ", g", 'font_size': 85},
         ),
-        row=1,
-        col=1,
     )
+    fig.update_layout(
+        height=200,
+        width=850,
+        title = {
+            'text': text_format.format(
+                "Antall virksomheter som har åpnet panel",
+                "i løpet av de siste 30 dagene"
+            ),
+            'x': 0.5,
+            'y': 1,
+        }
+    )
+    fig.show()
 
+    mål = 50
+    fig = go.Figure()
     fig.add_trace(
         go.Indicator(
             mode="number+gauge+delta",
-            gauge = {'shape': "bullet"},
-            delta = {'reference': 50},
-            value=antall_åpnet_kort_flere_dager,
-            number={"valueformat": ", g"},
-            title={
-                "text": "<br><span style='font-size:0.7em;color:gray'>{0}</span>".format(
-                    "Antall virksomheter som har åpnet panel flere dager"
-                )
+            gauge = {
+                'shape': "bullet",
+                'axis': {'range': [None, mål*1.2]},
+                'threshold': {
+                    'line': {'color': "red", 'width': 2},
+                    'thickness': 0.75,
+                    'value': mål
+                },
+                'bar': {'thickness': 0.5},
             },
+            delta = {'reference': mål},
+            value=antall_åpnet_kort_flere_dager,
+            number={"valueformat": ", g", 'font_size': 85},
         ),
-        row=2,
-        col=1,
     )
-    return fig
+    fig.update_layout(
+        height=250,
+        width=850,
+        title = {
+            'text': text_format.format(
+                "Antall virksomheter som har åpnet panel flere dager",
+                "i løpet av de siste 30 dagene"
+            ),
+            'x': 0.5,
+            'y': 1,
+        }
+    )
+    fig.show()
