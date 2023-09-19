aa7bfb1da89313cab889f2e4e32dbb64cfca5d13
Runar Skagestad <runska@gmail.com>
2023-09-19 10:17:03 +0200

    Modularisrte koden for å lage key result indikatorer

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from google.cloud.bigquery import Client
 from datetime import datetime, timedelta
 
 import plotly.graph_objs as go
-import plotly.subplots as sp
 
 import config
 
@@ -36,60 +35,61 @@ def plot_key_metrics(data, antall_dager: int):
         data, startdato, sluttdato
     )
 
-    text_format = "<br><span style='font-size:1em;color:gray'>{0}<br><span style='font-size:0.7em;color:gray'>{1}"
-
-    fig = go.Figure()
-    fig.add_trace(
-        go.Indicator(
-            mode="number",
-            value=antall_åpnet_kort,
-            number={"valueformat": ", g", "font_size": 85},
+    fig = make_key_result_indicator(
+        antall_åpnet_kort,
+        300,
+        formater_tittel(
+            "Antall virksomheter som har interagert med siden",
+            "i løpet av de siste 30 dagene",
         ),
     )
+    fig.show()
+
+    fig = make_key_result_indicator(
+        antall_åpnet_kort_flere_dager,
+        30,
+        formater_tittel(
+            "Antall virksomheter som har interagert med siden flere dager",
+            "i løpet av de siste 30 dagene",
+        ),
+    )
+    fig.show()
+
+
+def make_key_result_indicator(verdi, mål, tittel):
+    fig = go.Figure()
+    fig.add_trace(make_gauge(verdi, mål))
+
     fig.update_layout(
-        height=200,
+        height=300,
         width=850,
         title={
-            "text": text_format.format(
-                "Antall virksomheter som har åpnet panel",
-                "i løpet av de siste 30 dagene",
-            ),
+            "text": tittel,
             "x": 0.5,
             "y": 1,
         },
     )
-    fig.show()
+    return fig
 
-    mål = 50
-    fig = go.Figure()
-    fig.add_trace(
-        go.Indicator(
-            mode="number+gauge+delta",
-            gauge={
-                "shape": "bullet",
-                "axis": {"range": [None, mål * 1.2]},
-                "threshold": {
-                    "line": {"color": "red", "width": 2},
-                    "thickness": 0.75,
-                    "value": mål,
-                },
-                "bar": {"thickness": 0.5},
+
+def make_gauge(verdi, mål):
+    return go.Indicator(
+        mode="number+gauge+delta",
+        gauge={
+            "shape": "bullet",
+            "axis": {"range": [None, mål * 1.2]},
+            "threshold": {
+                "line": {"color": "red", "width": 2},
+                "thickness": 0.75,
+                "value": mål,
             },
-            delta={"reference": mål},
-            value=antall_åpnet_kort_flere_dager,
-            number={"valueformat": ", g", "font_size": 85},
-        ),
-    )
-    fig.update_layout(
-        height=250,
-        width=850,
-        title={
-            "text": text_format.format(
-                "Antall virksomheter som har åpnet panel flere dager",
-                "i løpet av de siste 30 dagene",
-            ),
-            "x": 0.5,
-            "y": 1,
+            "bar": {"thickness": 0.5},
         },
+        delta={"reference": mål},
+        value=verdi,
+        number={"valueformat": ", g", "font_size": 85},
     )
-    fig.show()
+
+
+def formater_tittel(overskrift, undertekst):
+    return f"<br><span style='font-size:1em;color:gray'>{overskrift}<br><span style='font-size:0.7em;color:gray'>{undertekst}"
